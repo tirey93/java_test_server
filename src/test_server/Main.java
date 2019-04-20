0216d8cfb8b1e41ed47323dfce1f9f72d19db71d
tirey93 <tirey93@gmail.com>
2019-04-20 14:36:08 +0200

    Init

---
+++ src/test_server/Main.java
@@ -0,0 +1,12 @@
+package test_server;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Test JAVY: \n");
+		for(int i = 0; i < 5; i++) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
